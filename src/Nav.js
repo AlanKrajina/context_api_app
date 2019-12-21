e94adc8df77db643c98b228c8f722e56a297d400
Alan Krajina <alankrajina88@gmail.com>
2019-12-21 13:40:24 +0100

    added data to Nav component - data sharing done

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import {MovieContext} from './MovieContext'
 
 const Nav = () => {
+
+    const value = useContext(MovieContext)
 
     return (
         <div>
             <h4>Alan</h4>
-            <p>Number of Movies: </p>
+            <p>Number of Movies: {value.length}</p>
         </div>
     )
 
